00477cd49b30734a0bd95ea8582ed1c85d7c4514
Vũ Việt Dũng <63959623+dungmax20@users.noreply.github.com>
2021-07-15 11:27:33 +0700

    Push bài tập

---
+++ BaiTap1/src/main/java/com/mycompany/baitap1/TestConnection.java
@@ -0,0 +1,52 @@
+package com.mycompany.baitap1;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+public class TestConnection {
+  
+    public static void main(String[]args) {
+        try{
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String url="jdbc:sqlserver://localhost:1433;databaseName=QLsinhvien;";
+            Connection con=DriverManager.getConnection(url,"sa","123");
+            System.out.println("Ket noi thanh cong");
+            Statement statement;
+            statement = con.createStatement();
+            String sql="Select masp,tensp,nhasanxuat,maloaisp from sanpham";
+            ResultSet rs = statement.executeQuery(sql);
+  
+            while (rs.next()) {
+                String masp = rs.getString("masp");
+                String tensp = rs.getString("tensp");
+                String nhasanxuat = rs.getString("nhasanxuat");
+                String maloaisp=rs.getString("maloaisp");
+                System.out.println("masp:" + masp);
+                System.out.println("tensp:" + tensp);
+                System.out.println("nhasanxuat:" + nhasanxuat);
+                System.out.println("maloaisp:" + maloaisp);
+            }
+            String sql2="Select maloaisp,tenloaisp from loaisanpham";
+            ResultSet rs2 = statement.executeQuery(sql2);
+  
+            while (rs2.next()) {
+          
+                String maloaisp=rs2.getString("maloaisp");
+                String tenloaisp=rs2.getString("tenloaisp");
+                System.out.println("--------------------");
+                System.out.println("tenloaisp"+tenloaisp);
+                System.out.println("maloaisp:" + maloaisp);
+            }
+        
+        }
+        catch(Exception e){
+             e.printStackTrace();
+        }
+    }
+        
+        
+    }
+      
+
+
